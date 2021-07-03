fa5f85df820fc2b70c7940e45971906313f50ead
cesar <ca2longoria@gmail.com>
2021-07-03 17:13:48 -0500

    yup

--- compary.lib.sh
+++ compary.lib.sh
@@ -4,13 +4,13 @@ function compare_grab_op() {
 	local GREP=
 	# Filter grep with args passed in as only available options.
 	[ $# -gt 0 ] && \
-		GREP=`echo $@ | sed 's/\s\s*/|/g; s/^/(/; s/$/)/'`
+		GREP=`echo $@ | sed 's/\s\s*/|/g'`
 	# Get the token before the current.
 	X=($COMP_CWORD-1)
 	local OP=${COMP_WORDS[$X]}
 	# Run the grep if a list of options were provided.
 	[ ! -z "$GREP" ] && \
-		OP=`echo $OP | grep -P "$GREP" || echo ''`
+		OP=`echo $OP | grep -P "($GREP)" || echo ''`
 	echo $OP
 }
 
